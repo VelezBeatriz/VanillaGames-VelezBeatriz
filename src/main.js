08341687cb822358a2734dd4aebd1713020faf97
Beatriz Vélez <velezsanchezbeatriz@fpllefia.com>
2024-01-15 19:11:45 +0100

    enrutador #13

--- src/main.js
+++ src/main.js
@@ -4,23 +4,30 @@ import 'bootstrap'
 // Import our custom CSS
 import './scss/style.scss'
 
+// Importamos la Función para detectar eventos al cargar las vistas
+import { enrutador } from './componentes/enrutador'
+
 // Import our components
 import { header } from '@components/header'
 import { footer } from '@components/footer'
 
 // Importamos la vista por defecto (que será home)
-async function cargarVista () {
-  const componente = await import('./vistas/homeVista')
-  const vista = componente.default
-  // Inyectamos la vista home
-  document.querySelector('main').innerHTML = vista.template
-  // Ejecutamos la lógica de la vista
-  vista.script()
-}
-cargarVista()
+// async function cargarVista () {
+//   const componente = await import('./vistas/homeVista')
+//   const vista = componente.default
+//   // Inyectamos la vista home
+//   document.querySelector('main').innerHTML = vista.template
+//   // Ejecutamos la lógica de la vista
+//   vista.script()
+// }
+// cargarVista()
 
 // Inyectamos el componente header
 document.querySelector('header').innerHTML = header.template
+header.script()
 
 // Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
+enrutador.observadorRutas()
+// Cargamos la página home
+window.location = '#/home'
