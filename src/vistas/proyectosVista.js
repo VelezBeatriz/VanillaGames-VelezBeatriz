4144091a491a5439bb82460c4a770ba1b28f23c5
Beatriz Vélez <velezsanchezbeatriz@fpllefia.com>
2024-01-26 16:35:39 +0100

    declaret db projects #14

--- src/vistas/proyectosVista.js
+++ src/vistas/proyectosVista.js
@@ -1,3 +1,4 @@
+import { proyectos } from '../bd/datosPruebas'
 export default {
   template: // html
     `
@@ -20,7 +21,7 @@ export default {
     <div class="border border-top-0 p-3">
         <div class="row">
             <div class="col-12 col-sm-4 mb-3">
-                <button class="btn btn-primary w-100">Subir proyecto</button>
+                <a href="#/proyectoNuevo" class="btn btn-primary w-100 router-link">Subir proyecto</a>
             </div>
             <div class="d-flex col-12 col-sm-8 mb-3">
                 <button class="btn btn-secondary me-2">
@@ -78,49 +79,8 @@ export default {
                         <th></th>
                     </tr>
                 </thead>
-
-                <tr>
-                    <td>
-                        <div class="containerImagen">
-                            <img width="200px" src="./images/juego.jpg" alt="" />
-                        </div>
-                    </td>
-                    <td>Proyecto ejemplo 1</td>
-                    <td>Ejemplo de web para centro fp</td>
-                    <td><a href="http://www.fpllefia.con">http://www.fpllefia.con</a></td>
-                    <td><a href="#">carrebola.github.com/juego1</a></td>
-                    <td>carlos</td>
-                    <td>13/12/2021</td>
-                    <td>?</td>
-                </tr>
-                <tr>
-                    <td>
-                        <div class="containerImagen">
-                            <img width="200px" src="./images/juego.jpg" alt="" />
-                        </div>
-                    </td>
-                    <td>Proyecto ejemplo 1</td>
-                    <td>Ejemplo de web para centro fp</td>
-                    <td><a href="http://www.fpllefia.con">http://www.fpllefia.con</a></td>
-                    <td><a href="#">carrebola.github.com/juego1</a></td>
-                    <td>carlos</td>
-                    <td>13/12/2021</td>
-                    <td>?</td>
-                </tr>
-                <tr>
-                    <td>
-                        <div class="containerImagen">
-                            <img width="200px" src="./images/juego.jpg" alt="" />
-                        </div>
-                    </td>
-                    <td>Proyecto ejemplo 1</td>
-                    <td>Ejemplo de web para centro fp</td>
-                    <td><a href="http://www.fpllefia.con">http://www.fpllefia.con</a></td>
-                    <td><a href="#">carrebola.github.com/juego1</a></td>
-                    <td>carlos</td>
-                    <td>13/12/2021</td>
-                    <td>?</td>
-                </tr>
+                <tbody id="#tbodyProyectos">
+                </tbody>
             </table>
         </div>
         <!-- tarjetas -->
@@ -345,5 +305,112 @@ export default {
         </div>
     </div>
 </div>
+    `,
+  script: () => {
+    // Función para pintar tabla a partir de array
+    const pintaTabla = (proyectos) => {
+      let tbodyProyectos = ''
+      // Para cada proyecto del array 'proyectos'
+      proyectos.forEach(proyecto => {
+        // sumamos un tr con los datos del proyecto
+        tbodyProyectos += `
+    <tr>
+      <td>
+        <div class="containerImagen">
+          <img 
+            width="200px" 
+            src=${proyecto.imagen || 'images/imagenVacia.png'} 
+            alt="imagen proyecto" />
+        </div>
+      </td>
+      <td>${proyecto.nombre}</td>
+      <td>${proyecto.descripcion}</td>
+      <td><a href="${proyecto.enlace}"><i class="bi bi-link fs-4"></i></a></td>
+      <td><a href="${proyecto.repositorio}"><i class="bi bi-folder-symlink fs-4"></i></a></td>
+      <td>${proyecto.nombre_usuario} ${proyecto.apellidos_usuario}</td>
+      <td>${proyecto.created_at}</td>
+      <td>${proyecto.estado}</td>
+      <td>
+        <a
+        data-user_id = ${proyecto.user_id}
+        class="d-none d-sm-inline btn btn-sm btn-outline-primary bi bi-pencil"
+        ></a>
+      </td>
+      <td>
+        <a
+          data-user_id = ${proyecto.user_id}
+          class="d-none d-sm-inline btn btn-sm btn-outline-danger bi bi-trash3"
+        ></a>
+      </td>
+    </tr>
+          
     `
+      })
+      // inyectamos el resultado en el tbody
+      //   console.log(tbodyProyectos)
+      document.querySelector('#tbodyProyectos').innerHTML = tbodyProyectos
+    }
+
+    // Ejecutamos la función
+    pintaTabla(proyectos)
+
+    // ####################################################################
+    // *** FUNCIÓN PARA PINTAR TABLA A PARTIR DE ARRAY datos ***
+    // ####################################################################
+    const pintaTarjetas = (proyectos) => {
+      let tarjetasProyectos = ''
+      // Para cada proyecto del array 'proyectos'
+      proyectos.forEach(proyecto => {
+        // sumamos un tr con los datos del proyecto
+        tarjetasProyectos += // html
+    `
+    <!-- tarjeta  -->
+    <div class="col-12 col-lg-6">
+      <div class="card mb-3">
+        <div class="row g-0">
+          <div
+            class="col-4"
+            style="
+              background-image: url(${proyecto.imagen || 'images/imagenVacia.png'});
+              background-position: center;
+              background-size: cover;
+            "
+          ></div>
+          <div class="col-8">
+            <div class="card-body">
+              <h5 class="card-title">${proyecto.nombre}</h5>
+              <p class="card-text">
+                ${proyecto.descripcion}
+              </p>
+              <p class="small m-0 text-end text-italic">Autor: ${proyecto.nombre_usuario} ${proyecto.apellidos_usuario}</p>
+              <p class="small text-end text-italic">Fecha: ${proyecto.created_at}</p>
+  
+              <a class="btn btn-sm btn-outline-primary" href="${proyecto.enlace}"><i class="bi bi-link"></i></a>
+              <a class="btn btn-sm btn-outline-primary" href="${proyecto.repositorio}"><i class="bi bi-folder-symlink"></i></a>
+              <a class="btn btn-sm btn-success" href="#">${proyecto.estado}</a>
+              <a
+                data-user_id = ${proyecto.user_id}
+                class="d-none d-sm-inline btn btn-sm btn-outline-primary bi bi-pencil"
+              ></a>
+              <a
+                data-user_id = ${proyecto.user_id}
+                class="d-none d-sm-inline btn btn-sm btn-outline-danger bi bi-trash3"
+              ></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  
+    `
+      })
+
+      document.querySelector('#tabTarjetas').innerHTML = tarjetasProyectos
+    }
+
+    // ####################################################################
+    // Función para pintar tarjetas
+    // ####################################################################
+
+    pintaTarjetas(proyectos)
+  }
 }
