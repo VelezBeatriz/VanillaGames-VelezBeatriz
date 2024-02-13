55be03efb5d5d0934dad41795c645f1686b994ea
Beatriz Vélez <velezsanchezbeatriz@fpllefia.com>
2024-02-13 20:24:53 +0100

    aply login and register #23 #24 #27

--- src/vistas/loginVista.js
+++ src/vistas/loginVista.js
@@ -1,69 +1,63 @@
 import { perfiles } from '../bd/datosPruebas'
-import { header } from '../components/header'
 import { ls } from '../components/funciones'
+import { header } from '../components/header'
+
 export default {
   template: // html
-      `
-      <div class="container">
-      <h1 class="mt-5 text-center">Inicia sesión</h1>
-      <div class="m-5 mx-auto" style="max-width: 400px">
-        <!-- Formulario de inicio de sesión (login) -->
-        <form novalidate action="" class="form border shadow-sm p-3">
-          <!-- Email -->
-          <label for="email" class="form-label">Email:</label>
-          <input required type="email" class="form-control" id="email" name="email" />
-          <div class="invalid-feedback">
-            El formato del email no es correcto
-          </div>
-          <!-- Contraseña -->
-          <label for="password" class="form-label mt-3">Contraseña:</label>
-          <input
-            required
-            id="password"
-            name="password"
-            type="password"
-            min="6"
-            class="form-control"
-          />
-          <div class="invalid-feedback">
-            La contraseña debe tener como mínimo 6 caracteres
-          </div>
-
-          <!-- Recordar contraseña -->
-          <div class="form-check mt-3">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckChecked"
-              checked
-            />
-            <label class="form-check-label" for="flexCheckChecked">
-              Recordar sesión
-            </label>
-          </div>
-
-          <!-- Link a recordar contraseña -->
-          <a class="d-block text-end" href="#"
-            >¿Has olvidado tu contraseña?</a
-          >
-
-          <!-- Botón Iniciar sesión -->
-          <input
-            type="submit"
-            class="btn btn-primary w-100 mt-3"
-            value="Iniciar sesión"
-          />
-        </form>
-        <a class="d-block mt-5 btn btn-secondary mx-auto" href="#"
-          >¿Eres nuevo? Regístrate</a
-        >
+  `
+  <div class="container">
+  <h1 class="mt-5 text-center">Inicia sesión</h1>
+  <div class="m-5 mx-auto" style="max-width: 400px">
+    <!-- Formulario de inicio de sesión (login) -->
+    <form id="formularioLogin" novalidate action="" class="form border shadow-sm p-3">
+      <!-- Email -->
+      <label for="emailLogin" class="form-label">Email:</label>
+      <input id="emailLogin" name="email" value="ejemplo@email.com" required type="email" class="form-control" />
+      <div class="invalid-feedback">El formato del email no es correcto</div>
+      <!-- Contraseña -->
+      <label for="passLogin" class="form-label mt-3">Contraseña:</label>
+      <input value="123456" id="passLogin" name="password" required minlength="6" type="password" class="form-control" />
+      <div class="invalid-feedback">
+        La contraseña debe tener como mínimo 6 caracteres
       </div>
-    </div>
-      `,
+
+      <!-- Recordar contraseña -->
+      <div class="form-check mt-3">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value=""
+          id="flexCheckChecked"
+          checked
+        />
+        <label class="form-check-label" for="flexCheckChecked">
+          Recordar sesión
+        </label>
+      </div>
+
+      <!-- Link a recordar contraseña -->
+      <a class="d-block text-end" href="#">¿Has olvidado tu contraseña?</a>
+
+      <!-- Botón Iniciar sesión -->
+      <input
+        type="submit"
+        class="btn btn-primary w-100 mt-3"
+        value="Iniciar sesión"
+      />
+    </form>
+
+    <a class="d-block mt-5 btn btn-secondary mx-auto" href="#"
+      >¿Eres nuevo? Regístrate</a
+    >
+  </div>
+</div>
+  `,
   script: () => {
+    console.log('vista login cargada')
+    // Validación bootstrap
+
     // Capturamos el formulario en una variable
-    const formulario = document.querySelector('form')
+    const formulario = document.querySelector('#formularioLogin')
     // Detectamos su evento submit (enviar)
     formulario.addEventListener('submit', (event) => {
       // Detenemos el evento enviar (submit)
@@ -73,8 +67,8 @@ export default {
       if (!formulario.checkValidity()) {
         // Y añadimos la clase 'was-validate' para que se muestren los mensajes
         formulario.classList.add('was-validated')
-        console.log('No valida')
       } else {
+        // Si valida enviamos los datos
         enviarDatos(formulario)
       }
     })
@@ -106,11 +100,11 @@ export default {
           // Actualizamos el header para que se muestren los menús que corresponden al rol
           header.script()
         } else {
-          // console.log('La contraseña no corresponde')
+          console.log('La contraseña no corresponde')
           alert('El usuario no existe o la contraseña no es correcta')
         }
       } else {
-        // console.log('El usuario no existe')
+        console.log('El usuario no existe')
         alert('El usuario no existe o la contraseña no es correcta')
       }
     }
