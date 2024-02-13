a92807f845c0f52aec190642d0d57f4b08a87bea
Beatriz Vélez <velezsanchezbeatriz@fpllefia.com>
2024-02-13 20:10:34 +0100

    add supabasejs #13

---
+++ src/components/supabase.js
@@ -0,0 +1,8 @@
+// console.log('PRUEBAS SUPABASE')
+
+const supabaseUrl = 'https://hndfwklbheorppxgouys.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuZGZ3a2xiaGVvcnBweGdvdXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4NjgyMzQsImV4cCI6MjAyMjQ0NDIzNH0.KSmZeBzCgadaRKW5SWHrjh_Nj6UvplQxV6WY_VKC2vA'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+// Mostramos por consola la conexión establecida
+console.log('conexión', supabase)
