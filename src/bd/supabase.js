e0386c293bb7e987952105d818dd7bd13243be85
Beatriz Vélez <velezsanchezbeatriz@fpllefia.com>
2024-02-15 18:45:15 +0100

    add const supabase export and passing 3/5 testb #28

--- src/bd/supabase.js
+++ src/bd/supabase.js
@@ -4,5 +4,7 @@ const supabaseUrl = 'https://hndfwklbheorppxgouys.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuZGZ3a2xiaGVvcnBweGdvdXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4NjgyMzQsImV4cCI6MjAyMjQ0NDIzNH0.KSmZeBzCgadaRKW5SWHrjh_Nj6UvplQxV6WY_VKC2vA'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+export { supabase }
+
 // Mostramos por consola la conexión establecida
 console.log('conexión', supabase)
